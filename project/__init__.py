d9613f773f0959d0230fde9b8249139f4f1a46bd
MohammadSatel <hamuds95@gmail.com>
2023-09-29 12:32:36 +0300

    changed folder structure

---
+++ project/__init__.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# DATABASE SETUP
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecret' #to allow us to use forms, not safe for deployment
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+# REGISTER BLUEPRINTS
+from project.core.views import core
+from project.books.views import books
+from project.customers.views import customers
+from project.loans.views import loans
+
+app.register_blueprint(core)
+app.register_blueprint(books)
+app.register_blueprint(customers)
+app.register_blueprint(loans)
---
+++ project/core/views.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import os
+import logging
+
+logging.basicConfig(filename=os.path.join(os.path.dirname(__file__), 'app.log'), level=logging.DEBUG)
+
+app = Flask(__name__)
+# app.template_folder = os.path.abspath('../front/templates')
+# app.static_folder = os.path.abspath('../front/static')
+
+@app.route('/')
+def index():
+    app.logger.info('Accessed homepage.')
+    return render_template('index.html')
+
+@app.route('/books')
+def books():
+    app.logger.info('Accessed books.')
+    return render_template('books.html')
+
+@app.route('/customers')
+def customers():
+    app.logger.info('Accessed customers.')
+    return render_template('customers.html')
+
+@app.route('/loans')
+def loans():
+    app.logger.info('Accessed loans.')
+    return render_template('loans.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
