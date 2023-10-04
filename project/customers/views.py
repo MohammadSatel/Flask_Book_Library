dec1cb83caf1b7ec35e2a89bc3395b4a11637cd3
MohammadSatel <hamuds95@gmail.com>
2023-10-04 16:40:14 +0300

    CRUD bugs

--- project/customers/views.py
+++ project/customers/views.py
@@ -1,9 +1,56 @@
-from flask import render_template, Blueprint, Flask
+from flask import render_template, Blueprint, request, redirect, url_for, jsonify
+from project import db
+from project.customers.models import Customer
+from project.customers.forms import CreateCustomer
 
-customers = Blueprint('customers', __name__, template_folder='templates', static_folder='static')
+customers = Blueprint('customers', __name__, template_folder='templates', url_prefix='/customers')
 
+# Route to display customers and handle CRUD actions
+@customers.route('/', methods=['GET'])
+def list_customers():
+    # Fetch all customers from the database
+    customers = Customer.query.all()
+    return render_template('customers.html', customers=customers)
 
-@customers.route('/customers')
-def index():
-    return render_template('customers.html')
+# Route to create a new customer
+@customers.route('/create', methods=['POST'])
+def create_customer():
+    data = request.form
+    new_customer = Customer(name=data['name'], author=data['author'], time=data['time'])
+    db.session.add(new_customer)
+    db.session.commit()
+    return redirect(url_for('customers.list_customers'))
 
+# Route to update an existing customer
+@customers.route('/<int:customer_id>/edit', methods=['POST'])
+def edit_customer(customer_id):
+    customer = Customer.query.get(customer_id)
+    if not customer:
+        return 'customer not found', 404
+
+    data = request.form
+    customer.name = data['name']
+    customer.author = data['author']
+    customer.time = data['time']
+
+    db.session.commit()
+    return redirect(url_for('customers.list_customers'))
+
+# Route to delete a customer
+@customers.route('/<int:customer_id>/delete', methods=['POST'])
+def delete_customer(customer_id):
+    customer = Customer.query.get(customer_id)
+    if not customer:
+        return 'customer not found', 404
+
+    db.session.delete(customer)
+    db.session.commit()
+    return redirect(url_for('customers.list_customers'))
+
+# Route to view a customer
+@customers.route('/<int:customer_id>')
+def view_customer(customer_id):
+    customer = Customer.query.get(customer_id)
+    if customer:
+        return render_template('view_customer.html', customer=customer)
+    return 'customer not found', 404
--- project/loans/views.py
+++ project/loans/views.py
@@ -1,9 +1,56 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, redirect, url_for, jsonify
+from project import db
+from project.loans.models import Loan
+from project.loans.forms import CreateLoan
 
-loans = Blueprint('loans', __name__, template_folder='templates', static_folder='static')
+loans = Blueprint('loans', __name__, template_folder='templates', url_prefix='/loans')
 
-@loans.route('/loans')
-def index():
-    return render_template('loans.html')
+# Route to display loans and handle CRUD actions
+@loans.route('/', methods=['GET'])
+def list_loans():
+    # Fetch all loans from the database
+    loans = Loan.query.all()
+    return render_template('loans.html', loans=loans)
 
-## CRUD FOR LOANS
+# Route to create a new loan
+@loans.route('/create', methods=['POST'])
+def create_loan():
+    data = request.form
+    new_loan = Loan(name=data['name'], author=data['author'], time=data['time'])
+    db.session.add(new_loan)
+    db.session.commit()
+    return redirect(url_for('loans.list_loans'))
+
+# Route to update an existing loan
+@loans.route('/<int:loan_id>/edit', methods=['POST'])
+def edit_loan(loan_id):
+    loan = Loan.query.get(loan_id)
+    if not loan:
+        return 'loan not found', 404
+
+    data = request.form
+    loan.name = data['name']
+    loan.author = data['author']
+    loan.time = data['time']
+
+    db.session.commit()
+    return redirect(url_for('loans.list_loans'))
+
+# Route to delete a loan
+@loans.route('/<int:loan_id>/delete', methods=['POST'])
+def delete_loan(loan_id):
+    loan = Loan.query.get(loan_id)
+    if not loan:
+        return 'loan not found', 404
+
+    db.session.delete(loan)
+    db.session.commit()
+    return redirect(url_for('loans.list_loans'))
+
+# Route to view a loan
+@loans.route('/<int:loan_id>')
+def view_loan(loan_id):
+    loan = Loan.query.get(loan_id)
+    if loan:
+        return render_template('view_loan.html', loan=loan)
+    return 'loan not found', 404
