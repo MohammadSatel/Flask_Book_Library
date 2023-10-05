203bae5f08e734288c56d1c5cd77bd139cc00245
MohammadSatel <hamuds95@gmail.com>
2023-10-05 10:37:13 +0300

    CRUD

--- project/customers/views.py
+++ project/customers/views.py
@@ -5,52 +5,134 @@ from project.customers.forms import CreateCustomer
 
 customers = Blueprint('customers', __name__, template_folder='templates', url_prefix='/customers')
 
-# Route to display customers and handle CRUD actions
+# Route to display customers in HTML
 @customers.route('/', methods=['GET'])
 def list_customers():
     # Fetch all customers from the database
     customers = Customer.query.all()
     return render_template('customers.html', customers=customers)
 
+# Route to fetch customers in JSON format
+@customers.route('/json', methods=['GET'])
+def list_customers_json():
+    # Fetch all customers from the database and convert to JSON
+    customers = Customer.query.all()
+    customer_list = [{'name': customer.name, 'author': customer.author, 'time': customer.time} for customer in customers]
+    return jsonify(customers=customer_list)
+
 # Route to create a new customer
-@customers.route('/create', methods=['POST'])
+@customers.route('/create', methods=['POST','GET'])
 def create_customer():
     data = request.form
+
+    # Validate the form data
+    if 'name' not in data or 'author' not in data or 'time' not in data:
+        return jsonify({'error': 'Invalid form data'}), 400
+
     new_customer = Customer(name=data['name'], author=data['author'], time=data['time'])
-    db.session.add(new_customer)
-    db.session.commit()
-    return redirect(url_for('customers.list_customers'))
+
+    try:
+        # Add the new customer to the session and commit to save to the database
+        db.session.add(new_customer)
+        db.session.commit()
+        return redirect(url_for('customers.list_customers'))
+    except Exception as e:
+        # Handle any exceptions, such as database errors
+        db.session.rollback()
+        return jsonify({'error': f'Error creating customer: {str(e)}'}), 500
 
 # Route to update an existing customer
 @customers.route('/<int:customer_id>/edit', methods=['POST'])
 def edit_customer(customer_id):
     customer = Customer.query.get(customer_id)
     if not customer:
-        return 'customer not found', 404
+        return jsonify({'error': 'customer not found'}), 404
 
     data = request.form
     customer.name = data['name']
     customer.author = data['author']
     customer.time = data['time']
 
-    db.session.commit()
-    return redirect(url_for('customers.list_customers'))
+    try:
+        # Update the customer in the database
+        db.session.commit()
+        return redirect(url_for('customers.list_customers'))
+    except Exception as e:
+        # Handle any exceptions, such as database errors
+        db.session.rollback()
+        return jsonify({'error': f'Error updating customer: {str(e)}'}), 500
 
 # Route to delete a customer
 @customers.route('/<int:customer_id>/delete', methods=['POST'])
 def delete_customer(customer_id):
     customer = Customer.query.get(customer_id)
     if not customer:
-        return 'customer not found', 404
+        return jsonify({'error': 'customer not found'}), 404
 
-    db.session.delete(customer)
-    db.session.commit()
-    return redirect(url_for('customers.list_customers'))
+    try:
+        # Delete the customer from the database
+        db.session.delete(customer)
+        db.session.commit()
+        return redirect(url_for('customers.list_customers'))
+    except Exception as e:
+        # Handle any exceptions, such as database errors
+        db.session.rollback()
+        return jsonify({'error': f'Error deleting customer: {str(e)}'}), 500
 
-# Route to view a customer
-@customers.route('/<int:customer_id>')
-def view_customer(customer_id):
-    customer = Customer.query.get(customer_id)
-    if customer:
-        return render_template('view_customer.html', customer=customer)
-    return 'customer not found', 404
+
+# from flask import render_template, Blueprint, request, redirect, url_for, jsonify
+# from project import db
+# from project.customers.models import Customer
+# from project.customers.forms import CreateCustomer
+
+# customers = Blueprint('customers', __name__, template_folder='templates', url_prefix='/customers')
+
+# # Route to display customers and handle CRUD actions
+# @customers.route('/', methods=['GET'])
+# def list_customers():
+#     # Fetch all customers from the database
+#     customers = Customer.query.all()
+#     return render_template('customers.html', customers=customers)
+
+# # Route to create a new customer
+# @customers.route('/create', methods=['POST'])
+# def create_customer():
+#     data = request.form
+#     new_customer = Customer(name=data['name'], author=data['author'], time=data['time'])
+#     db.session.add(new_customer)
+#     db.session.commit()
+#     return redirect(url_for('customers.list_customers'))
+
+# # Route to update an existing customer
+# @customers.route('/<int:customer_id>/edit', methods=['POST'])
+# def edit_customer(customer_id):
+#     customer = Customer.query.get(customer_id)
+#     if not customer:
+#         return 'customer not found', 404
+
+#     data = request.form
+#     customer.name = data['name']
+#     customer.author = data['author']
+#     customer.time = data['time']
+
+#     db.session.commit()
+#     return redirect(url_for('customers.list_customers'))
+
+# # Route to delete a customer
+# @customers.route('/<int:customer_id>/delete', methods=['POST'])
+# def delete_customer(customer_id):
+#     customer = Customer.query.get(customer_id)
+#     if not customer:
+#         return 'customer not found', 404
+
+#     db.session.delete(customer)
+#     db.session.commit()
+#     return redirect(url_for('customers.list_customers'))
+
+# # Route to view a customer
+# @customers.route('/<int:customer_id>')
+# def view_customer(customer_id):
+#     customer = Customer.query.get(customer_id)
+#     if customer:
+#         return render_template('view_customer.html', customer=customer)
+#     return 'customer not found', 404
--- project/loans/views.py
+++ project/loans/views.py
@@ -5,52 +5,76 @@ from project.loans.forms import CreateLoan
 
 loans = Blueprint('loans', __name__, template_folder='templates', url_prefix='/loans')
 
-# Route to display loans and handle CRUD actions
+# Route to display loans in HTML
 @loans.route('/', methods=['GET'])
 def list_loans():
     # Fetch all loans from the database
     loans = Loan.query.all()
     return render_template('loans.html', loans=loans)
 
+# Route to fetch loans in JSON format
+@loans.route('/json', methods=['GET'])
+def list_loans_json():
+    # Fetch all loans from the database and convert to JSON
+    loans = Loan.query.all()
+    loan_list = [{'name': loan.name, 'author': loan.author, 'time': loan.time} for loan in loans]
+    return jsonify(loans=loan_list)
+
 # Route to create a new loan
-@loans.route('/create', methods=['POST'])
+@loans.route('/create', methods=['POST','GET'])
 def create_loan():
     data = request.form
+
+    # Validate the form data
+    if 'name' not in data or 'author' not in data or 'time' not in data:
+        return jsonify({'error': 'Invalid form data'}), 400
+
     new_loan = Loan(name=data['name'], author=data['author'], time=data['time'])
-    db.session.add(new_loan)
-    db.session.commit()
-    return redirect(url_for('loans.list_loans'))
+
+    try:
+        # Add the new loan to the session and commit to save to the database
+        db.session.add(new_loan)
+        db.session.commit()
+        return redirect(url_for('loans.list_loans'))
+    except Exception as e:
+        # Handle any exceptions, such as database errors
+        db.session.rollback()
+        return jsonify({'error': f'Error creating loan: {str(e)}'}), 500
 
 # Route to update an existing loan
 @loans.route('/<int:loan_id>/edit', methods=['POST'])
 def edit_loan(loan_id):
     loan = Loan.query.get(loan_id)
     if not loan:
-        return 'loan not found', 404
+        return jsonify({'error': 'loan not found'}), 404
 
     data = request.form
     loan.name = data['name']
     loan.author = data['author']
     loan.time = data['time']
 
-    db.session.commit()
-    return redirect(url_for('loans.list_loans'))
+    try:
+        # Update the loan in the database
+        db.session.commit()
+        return redirect(url_for('loans.list_loans'))
+    except Exception as e:
+        # Handle any exceptions, such as database errors
+        db.session.rollback()
+        return jsonify({'error': f'Error updating loan: {str(e)}'}), 500
 
 # Route to delete a loan
 @loans.route('/<int:loan_id>/delete', methods=['POST'])
 def delete_loan(loan_id):
     loan = Loan.query.get(loan_id)
     if not loan:
-        return 'loan not found', 404
+        return jsonify({'error': 'loan not found'}), 404
 
-    db.session.delete(loan)
-    db.session.commit()
-    return redirect(url_for('loans.list_loans'))
-
-# Route to view a loan
-@loans.route('/<int:loan_id>')
-def view_loan(loan_id):
-    loan = Loan.query.get(loan_id)
-    if loan:
-        return render_template('view_loan.html', loan=loan)
-    return 'loan not found', 404
+    try:
+        # Delete the loan from the database
+        db.session.delete(loan)
+        db.session.commit()
+        return redirect(url_for('loans.list_loans'))
+    except Exception as e:
+        # Handle any exceptions, such as database errors
+        db.session.rollback()
+        return jsonify({'error': f'Error deleting loan: {str(e)}'}), 500
