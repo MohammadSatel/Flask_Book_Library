abb0141eed61d831157c61536bed21a61398ab06
MohammadSatel <hamuds95@gmail.com>
2023-10-06 23:05:23 +0300

    books axious

--- project/static/js/books.js
+++ project/static/js/books.js
@@ -1,162 +1,144 @@
-// Function to handle search
-function searchBooks() {
-    let input, filter, table, tr, td, i, j, txtValue;
-    input = document.getElementById("searchInput");
-    filter = input.value.toLowerCase();
-    table = document.querySelector(".table");
-    tr = table.getElementsByTagName("tr");
-    for (i = 1; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td");
-        let rowDisplay = false;
-        for (j = 0; j < td.length; j++) {
-            if (td[j]) {
-                txtValue = td[j].textContent || td[j].innerText;
-                if (txtValue.toLowerCase().indexOf(filter) > -1) {
-                    rowDisplay = true;
-                    break;
+document.addEventListener("DOMContentLoaded", () => {
+    // Function to handle search
+    function searchBooks() {
+        let input, filter, table, tr, td, i, j, txtValue;
+        input = document.getElementById("searchInput");
+        filter = input.value.toLowerCase();
+        table = document.querySelector(".table");
+        tr = table.getElementsByTagName("tr");
+        for (i = 1; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td");
+            let rowDisplay = false;
+            for (j = 0; j < td.length; j++) {
+                if (td[j]) {
+                    txtValue = td[j].textContent || td[j].innerText;
+                    if (txtValue.toLowerCase().indexOf(filter) > -1) {
+                        rowDisplay = true;
+                        break;
+                    }
                 }
             }
+            tr[i].style.display = rowDisplay ? "" : "none";
         }
-        tr[i].style.display = rowDisplay ? "" : "none";
     }
-}
 
-// Event listener for search input
-document.getElementById("searchInput").addEventListener("input", searchBooks);
+    // Event listener for search input
+    document.getElementById("searchInput").addEventListener("input", searchBooks);
 
-
-// Handle form submission for adding a new book
-$(document).ready(function() {
-    $('#addBookForm').submit(function(event) {
+    // Handle form submission for adding a new book
+    document.getElementById("addBookForm").addEventListener("submit", function(event) {
         event.preventDefault();  // Prevent the default form submission
 
         // Get form data
-        const name = $('#name').val();
-        const author = $('#author').val();
-        const year_published = $('#year_published').val();  // New field: Year Published
-        const book_type = $('#book_type').val();  // New field: Book Type
+        const name = document.getElementById('name').value;
+        const author = document.getElementById('author').value;
+        const year_published = document.getElementById('year_published').value;
+        const book_type = document.getElementById('book_type').value;
 
         // Send an AJAX request to create a new book
-        $.ajax({
-            url: '/books/create',
-            method: 'POST',
-            data: {
-                name: name,
-                author: author,
-                year_published: year_published,
-                book_type: book_type
-            },
-            success: function(response) {
-                console.log('Success:', response);
-                // Display a success notification
-                alert('Book added successfully!');
+        axios.post('/books/create', {
+            name: name,
+            author: author,
+            year_published: year_published,
+            book_type: book_type
+        })
+        .then(response => {
+            console.log('Success:', response.data);
+            // Display a success notification
+            alert('Book added successfully!');
 
-                // Close the modal
-                $('#addBookModal').modal('hide');
+            // Close the modal
+            document.getElementById('addBookModal').modal('hide');
 
-                // Reload the page to show the updated book list
-                location.reload();
-            },
-            error: function(error) {
-                console.log('Error:', error);
-                // Display an error notification
-                alert('Error adding book: ' + error.responseJSON.error);
-            }
+            // Reload the page to show the updated book list
+            location.reload();
+        })
+        .catch(error => {
+            console.log('Error:', error);
+            // Display an error notification
+            alert('Error adding book: ' + error.response.data.error);
         });
     });
-});
 
-// EDIT BOOK
-// EDIT BOOK
-function editBook(bookId) {
-    console.log('Edit button clicked for book ID:', bookId);
+    // EDIT BOOK
+    function editBook(bookId) {
+        console.log('Edit button clicked for book ID:', bookId);
 
-    // Fetch the existing book data for the given bookId via an AJAX request
-    $.ajax({
-        url: `/books/${bookId}/edit-data`,
-        method: 'GET',
-        success: function(response) {
-            console.log('Success:', response);
+        // Fetch the existing book data for the given bookId via an AJAX request
+        axios.get(`/books/${bookId}/edit-data`)
+            .then(response => {
+                console.log('Success:', response.data);
 
-            if (response.success) {
-                const bookData = response.book;
+                if (response.data.success) {
+                    const bookData = response.data.book;
 
-                // Pre-fill the modal form fields with the existing book data
-                $('#edit_name').val(bookData.name);
-                $('#edit_author').val(bookData.author);
-                $('#edit_year_published').val(bookData.year_published);
-                $('#edit_book_type').val(bookData.book_type);
+                    // Pre-fill the modal form fields with the existing book data
+                    document.getElementById('edit_name').value = bookData.name;
+                    document.getElementById('edit_author').value = bookData.author;
+                    document.getElementById('edit_year_published').value = bookData.year_published;
+                    document.getElementById('edit_book_type').value = bookData.book_type;
 
-                // Store the bookId in a data attribute of the save button
-                $('#saveEditBookButton').attr('data-book-id', bookId);
+                    // Store the bookId in a data attribute of the save button
+                    document.getElementById('saveEditBookButton').setAttribute('data-book-id', bookId);
 
-                // Show the modal for editing
-                $('#editBookModal').modal('show');
-            } else {
-                console.log('Error:', response.error);
-                alert('Error fetching book data: ' + response.error);
-            }
-        },
-        error: function(xhr, textStatus, errorThrown) {
-            console.log('Error:', errorThrown);
-            alert('Error fetching book data: ' + errorThrown);
-        }
-    });
-}
+                    // Show the modal for editing
+                    document.getElementById('editBookModal').modal('show');
+                } else {
+                    console.log('Error:', response.data.error);
+                    alert('Error fetching book data: ' + response.data.error);
+                }
+            })
+            .catch(error => {
+                console.log('Error:', error);
+                alert('Error fetching book data: ' + error);
+            });
+    }
 
-// Event listener for the edit form submission
-$(document).ready(function() {
-    $('#saveEditBookButton').click(function(event) {
-        const bookId = $(this).attr('data-book-id');
+    // Event listener for the edit form submission
+    document.getElementById('saveEditBookButton').addEventListener('click', function(event) {
+        const bookId = this.getAttribute('data-book-id');
 
         // Get form data
-        const name = $('#edit_name').val();
-        const author = $('#edit_author').val();
-        const yearPublished = $('#edit_year_published').val();
-        const bookType = $('#edit_book_type').val();
-
-        // Create a data object to send as JSON
-        const data = {
-            'name': name,
-            'author': author,
-            'year_published': yearPublished,
-            'book_type': bookType
-        };
+        const name = document.getElementById('edit_name').value;
+        const author = document.getElementById('edit_author').value;
+        const yearPublished = document.getElementById('edit_year_published').value;
+        const bookType = document.getElementById('edit_book_type').value;
 
         // Send an AJAX request to update the book data
-        $.ajax({
-            url: `/books/${bookId}/edit`,
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            success: function(response) {
-                console.log('Success:', response);
-                alert('Book updated successfully!');
-                $('#editBookModal').modal('hide');
-                location.reload();
-            },
-            error: function(xhr, textStatus, errorThrown) {
-                console.log('Error:', errorThrown);
-                alert('Error updating book: ' + errorThrown);
-            }
+        axios.post(`/books/${bookId}/edit`, {
+            name: name,
+            author: author,
+            year_published: yearPublished,
+            book_type: bookType
+        })
+        .then(response => {
+            console.log('Success:', response.data);
+            alert('Book updated successfully!');
+            document.getElementById('editBookModal').modal('hide');
+            location.reload();
+        })
+        .catch(error => {
+            console.log('Error:', error);
+            alert('Error updating book: ' + error);
         });
     });
+
+    // DEL BOOK
+    function deleteBook(bookId) {
+        axios.post(`/books/${bookId}/delete`)
+            .then(response => {
+                console.log('Success:', response.data);
+                alert('Book deleted successfully!');
+                location.reload();
+            })
+            .catch(error => {
+                console.log('Error:', error);
+                alert('Error deleting book: ' + error.response.data.error);
+            });
+    }
+
+    // Event listener for delete button click
+    function deleteButtonClick(bookId) {
+        deleteBook(bookId);
+    }
 });
-
-
-// DEL BOOK
-function deleteBook(bookId) {
-    $.ajax({
-        url: `/books/${bookId}/delete`,
-        method: 'POST',
-        success: function(response) {
-            console.log('Success:', response);
-            alert('Book deleted successfully!');
-            location.reload();
-        },
-        error: function(error) {
-            console.log('Error:', error);
-            alert('Error deleting book: ' + error.responseJSON.error);
-        }
-    });
-}
